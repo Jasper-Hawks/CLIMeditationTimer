bc7b87543ccaf724f63b45ad0fb0006b04ca6be2
jh <jasperhawks@gmail.com>
2021-05-04 18:39:00 -0400

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp meditate /usr/local/bin # Modify this directory path if your PATH has different directories
+cp GongSound.mp3 ~/Music # Modify this directory path if you'd like to place the file somewhere else
+# REMEMBER to also modify the meditate script if you change this path
+
+echo Successfully added meditate to the /usr/local/bin directory and GongSound to the ~/Music directory
+
